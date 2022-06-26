1fdc8ac1893ec647d6daf1ebbb6eef3314c8e78c
Vaidotak <37157652+Vaidotak@users.noreply.github.com>
2022-06-26 17:53:19 +0300

    Pakeisti ciklai

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] numbers = new int[6];
-        for (int i = 0; i < numbers.length; i++) {
+        int i = 0;
+        while (i < numbers.length) { /* buvo for, bet intellij pasiūlė keisti į while. Kažkas panašaus į for, tik veiksmai išmėtyti į tris eilutes */
             System.out.println("Įvesk skaičių: ");
             numbers[i] = input.nextInt();
-
+            i++;
         }
+
+        input.close();
+
         String masyvas = Arrays.toString(numbers);
+
         System.out.println("Nerūšiuotas masyvas: " + masyvas);
-        System.out.println("Bubble sort: "); //ne geriausias sprendimas, bet tiek to
-        int len = numbers.length;
-        bubbleSort(numbers,len);
-        for(int i = 0; i<len; ++i){
-            System.out.print(numbers[i] + ", ");
+        System.out.println("Bubble sort išrūšiuotas: "); //ne geriausias sprendimas, bet tiek to
+
+        int numbersLenght = numbers.length;
+        bubbleSort(numbers, numbersLenght);
+        for (int arrNumber : numbers) { // vietoje įprasto for intellij pasiūlė kažkokį paprastesnį for
+            System.out.print(arrNumber + ", ");
         }
     }
 
-    public static void bubbleSort(int[] sort_arr, int len){
+    public static void bubbleSort(int[] sort_arr, int numbersLenght) {
 
-        for (int i=0;i<len-1;++i){
+        for (int i = 0; i < numbersLenght - 1; ++i) {
 
-            for(int j=0;j<len-i-1; ++j){
+            for (int j = 0; j < numbersLenght - i - 1; ++j) {
 
-                if(sort_arr[j+1]<sort_arr[j]){
+                if (sort_arr[j + 1] < sort_arr[j]) {
 
                     int swap = sort_arr[j];
-                    sort_arr[j] = sort_arr[j+1];
-                    sort_arr[j+1] = swap;
+                    sort_arr[j] = sort_arr[j + 1];
+                    sort_arr[j + 1] = swap;
 
                 }
             }
